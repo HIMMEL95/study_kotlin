bad2cee2232d92eb34087f763aac561be962a42a
HIMMEL <bbluesky7738@gmail.com>
2022-08-22 22:53:27 +0900

    first

--- Baekjoon/if_/Oven.kt
+++ Baekjoon/if_/Oven.kt
@@ -1,20 +1,23 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    var h = readLine()!!.toInt()
-    var m = readLine()!!.toInt()
-    var need_m = readLine()!!.toInt()
+    val sc: Scanner = Scanner(System.`in`)
+    var h = sc.nextInt()
+    var m = sc.nextInt()
+    val need_m = sc.nextInt()
 
     if (m + need_m >= 60) {
-        h++
-        m = (m + need_m) - 60
-        if (m >= 60) {
-            h++
-            m = (m + need_m) - 60
+        val plusHour = (m + need_m) / 60
+        m = (m + need_m) % 60
+        if (h + plusHour >= 24) {
+            h = (h + plusHour) % 24
+        } else {
+            h += plusHour
         }
     } else {
         m += need_m
     }
 
-    println(h)
-    println(m)
+    println("${h} ${m}")
 
 }
